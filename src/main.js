90f413e7eb5a80eac7d6afd7b3c065f131504e2b
Átila <atilalimade@gmail.com>
2025-05-06 20:04:23 -0300

    front: implementing basis of the script
    working fine!

--- src/main.js
+++ src/main.js
@@ -1,36 +1,66 @@
 let gifElement = document.getElementById("main-gif")
 let messageElement = document.getElementById("client-message")
 
+//* App states: 1 state = 1 stage, with images and messages accordingly
 let currentState // "accept" | "denial" 
 let flowStates = {
     "denial": 0,
     "accept": 0
 }
+let maxFlowStates = {
+    "denial": 0,
+    "accept": 0
+}
 const imagePaths = {
     "denial": {
-        "stage-1": "dist/images/denial/stage-1.gif",
-        "stage-2": "dist/images/denial/stage-2.gif",
-        "stage-3": "dist/images/denial/stage-3.gif",
-        "stage-4": "dist/images/denial/stage-4.jpg", 
+        "stage-1": "./images/denial/stage-1.gif",
+        "stage-2": "./images/denial/stage-2.gif",
+        "stage-3": "./images/denial/stage-3.gif",
+        "stage-4": "./images/denial/stage-4.jpg", 
     },
     "accept": {
-        "stage-1": "dist/images/accept/stage-1.gif",
-        "stage-2": "dist/images/accept/stage-2.gif",
-        "stage-3": "dist/images/accept/stage-3.gif",
+        "stage-1": "./images/accept/stage-1.gif",
+        "stage-2": "./images/accept/stage-2.gif",
+        "stage-3": "./images/accept/stage-3.gif",
     }
 }
 const clientMessages = {
     "denial": {
-        "stage-1": "#1",
-        "stage-2": "#2",
-        "stage-3": "#3",
-        "stage-4": "#4"
+        "stage-1": "Pera, como assim você não gosta?",
+        "stage-2": "Como assim?? assim vc me deixa triste",
+        "stage-3": "Para, você tá sendo mt ruim :(",
+        "stage-4": ""
     },
     "accept": {
-        "stage-1": "#1",
-        "stage-2": "#2",
-        "stage-3": "#3",
+        "stage-1": "Quer dizer que você gosta muito de mi?",
+        "stage-2": "Sério?? muito mesmo?",
+        "stage-3": "Te amo mais sua fofinha linda <3",
     }
+}
+
+/**
+ * Check Flow State to generate future app stages
+ *
+ * @param {string} choice - can receive strings 'accept' or 'denial'
+ * @returns {number}  
+ */
+function applyFlowState(choice) {
+    switch(choice){
+        case "denial":
+            if (flowStates[choice] !== Object.keys(clientMessages[choice]).length){
+                flowStates[choice]++
+            }
+            break;
+        case "accept":
+            if (flowStates[choice] !== Object.keys(clientMessages[choice]).length){
+                flowStates[choice]++
+            }
+            break;
+        default:
+            throw Error("Not allowed choice at applyFlowState:" + choice)
+    }
+
+    return flowStates[choice]
 }
 
 /**
@@ -39,17 +69,16 @@ const clientMessages = {
  * @param {string} choice - can receive strings 'accept' or 'denial'
  */
 const applyChoice = (choice) => {
-    if ( !(choice === "denial" && flowStates[choice] !== 4)
-        && 
-         !(choice === "accept" && flowStates[choice] !== 3))
+    if (!["denial", "accept"].includes(choice)){
+        throw Error("Not allowed choice at applyChoice" + choice)
+    } 
+    
+    let currentNumber = applyFlowState(choice)
 
-    flowStates[choice] += 1
-    let stageToApply = "stage-" + flowStates[choice]
+    let stageToApply = "stage-" + currentNumber
     
     let imageToApply = imagePaths[choice][stageToApply]
-    console.log(imageToApply)
-    let messageToSend = currentState[choice][stageToApply]
-    console.log(messageToSend)
+    let messageToSend = clientMessages[choice][stageToApply]
 
     //modify
     gifElement.src = imageToApply
@@ -57,5 +86,4 @@ const applyChoice = (choice) => {
 
     //finally
     currentState = choice
-    alert("user choice: ", choice)
 }
